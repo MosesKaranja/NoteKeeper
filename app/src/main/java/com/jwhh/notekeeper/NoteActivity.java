c149f6fc5da5d83553cb4cc4674c35bcc824928a
unknown <moses.karanja@strathmore.edu>
2022-07-10 13:41:31 +0300

    Commit 1

---
+++ app/src/main/java/com/jwhh/notekeeper/NoteActivity.java
@@ -0,0 +1,151 @@
+package com.jwhh.notekeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class NoteActivity extends AppCompatActivity {
+    public static final String NOTE_POSITION = "com.jwhh.notekeeper.NOTE_INFO";
+    public static final int POSITION_NOT_SET = -1;
+
+    private NoteInfo mNote;
+    Spinner spinnerCourses;
+    List<CourseInfo> courses;
+    ArrayAdapter<CourseInfo> adapterCourses;
+    private boolean mIsNewNote;
+
+    EditText textNoteTitle, textNoteText;
+    private int notePosition;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                startActivity(new Intent(getApplicationContext(), NoteActivity.class));
+            }
+        });
+
+        spinnerCourses = findViewById(R.id.spinner);
+        courses = DataManager.getInstance().getCourses();
+        adapterCourses = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCourses.setAdapter(adapterCourses);
+
+        readDisplayStateValues();
+
+         textNoteTitle = findViewById(R.id.editText_title);
+         textNoteText = findViewById(R.id.editTextTextMultiLine2);
+
+        if (!mIsNewNote)
+            displayNote(spinnerCourses, textNoteTitle, textNoteText);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_send_email) {
+            sendEmail();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText){
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        int courseIndex = courses.indexOf(mNote.getCourse());
+        spinnerCourses.setSelection(courseIndex);
+        textNoteTitle.setText(mNote.getTitle());
+        textNoteText.setText(mNote.getText());
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveNote();
+    }
+
+    private void saveNote() {
+        mNote.setCourse((CourseInfo) spinnerCourses.getSelectedItem());
+        mNote.setTitle(textNoteTitle.getText().toString());
+        mNote.setText(textNoteText.getText().toString());
+
+        Toast.makeText(this, "Note Saved Successfully", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void readDisplayStateValues(){
+        Intent intent = getIntent();
+        int position = intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET);
+
+        mIsNewNote = position == POSITION_NOT_SET;
+
+        if (mIsNewNote){
+            createNewNote();
+
+        }
+        else {
+            mNote = DataManager.getInstance().getNotes().get(position);
+
+        }
+
+
+    }
+
+    private void createNewNote() {
+        DataManager dm = DataManager.getInstance();
+        notePosition = dm.createNewNote();
+        mNote = dm.getNotes().get(notePosition);
+    }
+
+    private void sendEmail(){
+        CourseInfo course = (CourseInfo) spinnerCourses.getSelectedItem();
+        String subject  = textNoteTitle.getText().toString();
+        String text = "Checkout what I learned in the pluralsight course \"" + course.getTitle() + "\"\n" + textNoteText.getText();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc2822");
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/jwhh/notekeeper/NoteList.java
@@ -0,0 +1,76 @@
+package com.jwhh.notekeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import java.util.List;
+
+public class NoteList extends AppCompatActivity {
+    public static final String NOTE_INFO = "com.jwhh.notekeeper.NOTE_INFO";
+
+    private AppBarConfiguration appBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_note_list);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                startActivity(new Intent(getApplicationContext(), NoteActivity.class));
+            }
+        });
+
+        initializeDisplayContent();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_note_list);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    private void initializeDisplayContent(){
+        ListView listNotes = findViewById(R.id.list_notes);
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+        listNotes.setAdapter(adapterNotes);
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(getApplicationContext(), "Clicked an Item", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), NoteActivity.class);
+
+//                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
+                intent.putExtra(NoteActivity.NOTE_POSITION, position);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
